775d5e28cc8de53e4dcce36e8d43ba8e8c270dbc
Juan Mutiz <juan.mutiz.t0126452@edu.atec.pt>
2025-02-20 10:56:44 +0000

    exercicio 1 passado pelo formador

---
+++ UFCD10789/exerciciosProf/ex1/ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+int ns;
+
+printf("Segundos: ");
+scanf("%d",&ns);
+
+int nm=ns/60;
+int nh=nm/60;
+
+printf("segundos: %d,minutos: %d,Horas:%d",ns,nm,nh);
+
+
+}
