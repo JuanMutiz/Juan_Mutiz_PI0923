1408c77c5f825bf2ecd009abd87b6451ffa9ad7e
Juan Mutiz <Juan.Mutiz.t0126452@edu.atec.pt>
2025-04-15 15:19:11 +0100

    exercicio 1 e 2 concluidos, ver o exercicio 2 para o exercicio 11

---
+++ UFCD10789/exerciciosPy/aula2/challengesDario/ex2.py
@@ -0,0 +1,16 @@
+for i in range(10):
+    numero = int(input(f"Digite o {i+1}º número: "))
+    
+    if numero % 2 == 0:
+        print(f"O número {numero} é par.")
+    else:
+        print(f"O número {numero} é ímpar.")
+
+'''while parar < 11:
+    if insereNum % 2 == 0:
+        numero.append(insereNum)
+        print(f"O número {numero} é par.")
+    else:
+        numero.append(insereNum)
+        print(f"O número {numero} é ímpar.")
+    parar += 1 '''
