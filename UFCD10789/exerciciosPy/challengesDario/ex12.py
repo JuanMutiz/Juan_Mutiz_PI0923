7b4705a04f97e5bc95636f5fafe4cac2c756476a
Juan Mutiz <147599782+JuanMutiz@users.noreply.github.com>
2025-04-24 09:32:34 +0100

    Update ex12.py

--- UFCD10789/exerciciosPy/challengesDario/ex12.py
+++ UFCD10789/exerciciosPy/challengesDario/ex12.py
@@ -2,17 +2,25 @@ while True:
     try:
         num = int(input("Insira um número: "))
         print()
-
+        if num <= 1:
+            print("Por favor, insira um número maior que 1.\n")
+            continue
     except ValueError:
-        print("\nInsira um valor válido!\n")
-
+        print("\nInsira um valor válido.\n")
         continue
+
+    operacoes = 0
 
     for i in range(1, num):
         print(f"{num} + {i} = {num + i}")
+        operacoes += 1
         print(f"{num} - {i} = {num - i}")
+        operacoes += 1
         print(f"{num} * {i} = {num * i}")
+        operacoes += 1
         print(f"{num} / {i} = {num / i}")
+        operacoes += 1
         print()
 
+    print(f"Total de operações efetuadas: {operacoes}")
     break
